66e393547fd66b41b4ce13db3a45f30b464bfe2d
bellec1u <leopold.bellec@gmail.com>
2017-11-27 22:46:51 +0100

    [ADD] navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  private title = 'iMatchProfile';
+
   constructor() { }
 
   ngOnInit() {
