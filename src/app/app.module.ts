5727bde3c3ba39ab2aebd94ca2893eacd9453af0
Zhephyr54 <ale.lanoix@gmail.com>
2017-12-01 13:38:40 +0100

    [ADD] Profil candidat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ListPostComponent } from './list-post/list-post.component';
 import { ListPostService } from './list-post/list-post.service';
+import { CandidateProfileComponent } from './candidate-profile/candidate-profile.component';
+import {PostComponent} from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ListPostComponent
+    ListPostComponent,
+    PostComponent,
+    CandidateProfileComponent
   ],
   imports: [
     BrowserModule
