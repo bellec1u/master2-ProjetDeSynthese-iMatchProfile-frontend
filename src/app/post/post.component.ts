52b4e0211620047e5187b76b68a44a1501a53c8c
dyasar <yasarfb66@hotmail.fr>
2017-12-08 19:46:53 +0100

    first version of post

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PostService} from '../shared/services/post-services/post.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -6,11 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  private _post;
 
-  constructor() { }
+
+  constructor(private _postService: PostService,private _route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this._route.params
+      .filter(params => !!params['id'])
+      .flatMap(params => this._postService.fetchOne(params['id']))
+      .subscribe((post: any) => {
+          this._post = post
+          console.log(this._post);
+        }
+      );
   }
+
+    get post() {
+      return this._post;
+    }
 
   Postuler() {}
   Contacter() {}
