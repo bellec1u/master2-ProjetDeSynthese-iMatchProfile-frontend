bbb551483c1599e0dcd1a6c48a005e49459921b1
bellec1u <leopold.bellec@gmail.com>
2017-12-29 22:00:07 +0100

    add crud post test

--- e2e/post.e2e-spec.ts
+++ e2e/post.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { PostPage } from './post.po';
-import {browser} from 'protractor';
+import {browser, by, element, protractor} from 'protractor';
 
-describe('imp home view', function() {
+describe('imp post view', function() {
   let page: PostPage;
 
   beforeEach(() => {
@@ -9,14 +9,91 @@ describe('imp home view', function() {
   });
 
   it('should click on the see profil and redirect to profil page', () => {
-    page.navigateTo();
-    page.getSeeButton().click();
-    expect(browser.getCurrentUrl()).toEqual('http://localhost:49152/profile/1');
+    // page.navigateToRecruiterPosts();
+    // page.getSeeButton().click();
+    // expect(browser.getCurrentUrl()).toEqual('http://localhost:49152/profile/1');
   });
 
   it('should click on the delete and redirect to home page', () => {
-    page.navigateTo();
-    page.getDeleteButton().click();
-    expect(browser.getCurrentUrl()).toEqual('http://localhost:49152/home');
+    // page.navigateToRecruiterPosts();
+    // page.getDeleteButton().click();
+    // expect(browser.getCurrentUrl()).toEqual('http://localhost:49152/home');
+  });
+
+  it('should see all my posts, can create one post, update it and delete it', () => {
+    // go to the page that contains all the posts available (recruiter 7)
+    page.navigateToRecruiterPosts();
+    // initially, there should be 2 posts
+    expect(page.getAllPosts().count()).toEqual(2);
+    // click on the "Ajouter un poste" button
+    page.getAddPostButton().click();
+    // fill the form and send it
+    browser.wait(protractor.ExpectedConditions.visibilityOf(page.getPostReferenceInput()));
+    page.getPostReferenceInput().sendKeys('test reference');
+    page.getPostTitleInput().sendKeys('test title');
+    page.getPostExperienceInput().sendKeys('test experience');
+    page.getPostSalaryIndexInput().sendKeys('test salaryIndex');
+    page.getPostMinSalaryInput().sendKeys(1);
+    page.getPostMaxSalaryInput().sendKeys(2);
+    page.getPostContractTypeInput().sendKeys('test contractType');
+    page.getPostDescriptionInput().sendKeys('test description');
+    page.getPostImpotantNotesInput().sendKeys('test importantNotes');
+    page.getPostWorkplaceInput().sendKeys('test workplace');
+    page.getPostOrganizationInput().sendKeys('test organization');
+    page.getPostWorkUnitInput().sendKeys('test workUnit');
+    page.getPostAddSkillButton().click();
+    page.getSendFormButton().click();
+    // go to the page that contains all the posts available (recruiter 7)
+    page.navigateToRecruiterPosts();
+    // there should be 3 posts
+    browser.wait(protractor.ExpectedConditions.visibilityOf(page.getAllPosts().get(0)));
+    expect(page.getAllPosts().count()).toEqual(3);
+    // see the new post and check its content
+    page.getLastPostId().getText().then(function(id) {
+      page.getAllSeePostButton().last().click();
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:49152/post/' + id);
+    });
+    expect(page.getPostReference().getText()).toEqual('test reference');
+    expect(page.getPostTitle().getText()).toEqual('test title');
+    expect(page.getPostExperience().getText()).toEqual('test experience');
+    expect(page.getPostSalaryIndex().getText()).toEqual('test salaryIndex');
+    expect(page.getPostContractType().getText()).toEqual('test contractType');
+    expect(page.getPostDescription().getText()).toEqual('test description');
+    expect(page.getPostImpotantNotes().getText()).toEqual('test importantNotes');
+    expect(page.getPostWorkplace().getText()).toEqual('test workplace');
+    expect(page.getPostOrganization().getText()).toEqual('test organization');
+    expect(page.getPostWorkUnit().getText()).toEqual('test workUnit');
+    // go to the page that contains all the posts available (recruiter 7)
+    page.navigateToRecruiterPosts();
+    // update the new post (change its title) and validate
+    browser.wait(protractor.ExpectedConditions.visibilityOf(page.getAllPosts().get(0)));
+    page.getAllUpdatePostButton().last().click();
+    page.getPostTitleInput().sendKeys('2');
+    page.getSendFormButton().click();
+    // go to the page that contains all the posts available (recruiter 7)
+    page.navigateToRecruiterPosts();
+    // see the poste updated and see the modification
+    browser.wait(protractor.ExpectedConditions.visibilityOf(page.getAllPosts().get(0)));
+    page.getLastPostId().getText().then(function(id) {
+      page.getAllSeePostButton().last().click();
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:49152/post/' + id);
+    });
+    expect(page.getPostReference().getText()).toEqual('test reference');
+    expect(page.getPostTitle().getText()).toEqual('test title2');
+    expect(page.getPostExperience().getText()).toEqual('test experience');
+    expect(page.getPostSalaryIndex().getText()).toEqual('test salaryIndex');
+    expect(page.getPostContractType().getText()).toEqual('test contractType');
+    expect(page.getPostDescription().getText()).toEqual('test description');
+    expect(page.getPostImpotantNotes().getText()).toEqual('test importantNotes');
+    expect(page.getPostWorkplace().getText()).toEqual('test workplace');
+    expect(page.getPostOrganization().getText()).toEqual('test organization');
+    expect(page.getPostWorkUnit().getText()).toEqual('test workUnit');
+    // go to the page that contains all the posts available (recruiter 7)
+    page.navigateToRecruiterPosts();
+    // delete the new post
+    browser.wait(protractor.ExpectedConditions.visibilityOf(page.getAllPosts().get(0)));
+    page.getLastPostDeleteButton().click();
+    // there should be 2 posts
+    expect(page.getAllPosts().count()).toEqual(2);
   });
 });
--- e2e/post.po.ts
+++ e2e/post.po.ts
@@ -1,15 +1,155 @@
 import { browser, by, element } from 'protractor';
 
 export class PostPage {
-  navigateTo() {
-    return browser.get('/post/15');
+  navigateToRecruiterPosts() {
+    return browser.get('/recruiterPost/7');
   }
 
-  getDeleteButton() {
-    return element(by.css('.btn-danger'));
+  /**
+   * Return all posts of the current recruiter
+   *
+   * @returns {ElementArrayFinder}
+   */
+  getAllPosts() {
+    return element.all(by.id('post'));
   }
 
-  getSeeButton() {
-    return element(by.css('.btn-sm'));
+  /**
+   * Return the id of the last post added
+   *
+   * @returns {ElementFinder}
+   */
+  getLastPostId() {
+    return this.getAllPosts().last().element(by.id('postTitle'));
   }
+
+  getLastPostDeleteButton() {
+    return this.getAllPosts().last().element(by.css('.btn-danger'));
+  }
+
+  /**
+   * Return all button which enable to see one post
+   *
+   * @returns {ElementArrayFinder}
+   */
+  getAllSeePostButton() {
+    return element.all(by.css('.btn-primary'));
+  }
+
+  /**
+   * Return all button which enable to see the update post mode
+   *
+   * @returns {ElementArrayFinder}
+   */
+  getAllUpdatePostButton() {
+    return element.all(by.css('.btn-warning'));
+  }
+
+  /**
+   * Return the button for add a new post
+   *
+   * @returns {ElementFinder}
+   */
+  getAddPostButton() {
+    return element(by.id('addPostButton'));
+  }
+
+  // ---------- ---------- ---------- post elements
+  getPostReference() {
+    return element(by.id('reference'));
+  }
+
+  getPostTitle() {
+    return element(by.id('title'));
+  }
+
+  getPostExperience() {
+    return element(by.id('experience'));
+  }
+
+  getPostSalaryIndex() {
+    return element(by.id('salaryIndex'));
+  }
+
+  getPostContractType() {
+    return element(by.id('contractType'));
+  }
+
+  getPostDescription() {
+    return element(by.id('description'));
+  }
+
+  getPostImpotantNotes() {
+    return element(by.id('importantNotes'));
+  }
+
+  getPostWorkplace() {
+    return element(by.id('workplace'));
+  }
+
+  getPostOrganization() {
+    return element(by.id('organization'));
+  }
+
+  getPostWorkUnit() {
+    return element(by.id('workUnit'));
+  }
+
+  // ---------- ---------- ---------- form post modal elements
+  getPostReferenceInput() {
+    return element(by.id('reference'));
+  }
+
+  getPostTitleInput() {
+    return element(by.id('title'));
+  }
+
+  getPostExperienceInput() {
+    return element(by.id('experience'));
+  }
+
+  getPostSalaryIndexInput() {
+    return element(by.id('salaryIndex'));
+  }
+
+  getPostMinSalaryInput() {
+    return element(by.id('minSalary'));
+  }
+
+  getPostMaxSalaryInput() {
+    return element(by.id('maxSalary'));
+  }
+
+  getPostContractTypeInput() {
+    return element(by.id('contractType'));
+  }
+
+  getPostDescriptionInput() {
+    return element(by.id('description'));
+  }
+
+  getPostImpotantNotesInput() {
+    return element(by.id('importantNotes'));
+  }
+
+  getPostWorkplaceInput() {
+    return element(by.id('workplace'));
+  }
+
+  getPostOrganizationInput() {
+    return element(by.id('organization'));
+  }
+
+  getPostWorkUnitInput() {
+    return element(by.id('workUnit'));
+  }
+
+  getPostAddSkillButton() {
+    return element(by.id('addSkill'));
+  }
+
+  getSendFormButton() {
+    return element(by.id('sendForm'));
+  }
+
 }
